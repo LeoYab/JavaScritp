51fe9d29f4bdad1d82816955fd1521bb6b34529f
Leonel Yaborski <leonel.yaborski@gmail.com>
2022-08-05 09:45:01 -0300

    Se muestra cantidad total en pantalla

--- js/main.js
+++ js/main.js
@@ -10,17 +10,55 @@ let totProd;
 let contProdNom = 0;
 let fecha = new Date();
 
-/* alert("¡Bienvenido a SuperLista!\n\nAquí podrás crear tu lista de super y visualizar el total." + "\n\nFecha: " + fecha.toLocaleString()); */
+
+function prodGen() {
+
+    prodIng = document.getElementById("cantProd").value = "";
+    precIng = document.getElementById("precIngr").value = "";
+    cantIng = document.getElementById("cantIngr").value = "";
+
+    let genTab = document.createElement("tr");
+
+    for (const productor of productos) {
+
+        genTab.innerHTML =`
+    <td>${productor.nombre}</td>
+    <td>$${productor.precio}</td>
+    <td class="canti">
+      <div class="input-group input-group-sm ">
+        <span class="input-group-text" id="basic-addon1">X</span>
+        <input type="number" class="form-control text-center" placeholder="${productor.cantidad}" aria-describedby="basic-addon1">
+      </div>
+    </td>
+    <td>$${productor.totprod}</td>
+    <td>
+      <div class="d-flex justify-content-end gap-2">
+        <button class="btn btn-sm btn-outline-success" type="submit">Edit</button>
+        <button class="btn btn-sm btn btn-outline-secondary" type="submit">Del</button>
+      </div>
+    </td>
+    `;
+    document.getElementById("totalProd").innerHTML = "TOTAL: $" + precioTotal;
+
+    }
+
+  /*   precioTotal = precioTotal + producto.totprod; */
+  
+    tabGen = document.getElementById("tabla");
+    tabGen.append(genTab);
+
+
+}
+
+
 
 function addProd() {
-
-    btnAgregarOcul()
 
     let prodIng = document.getElementById("cantProd").value;
     let precIng = document.getElementById("precIngr").value;
     let cantIng = document.getElementById("cantIngr").value;
 
-
+    
 
     class Producto {
         constructor(nombre, precio, cantidad) {
@@ -29,62 +67,46 @@ function addProd() {
             this.precio = parseFloat(precio);
             this.cantidad = parseInt(cantidad);
             this.totprod = parseFloat(this.precio * this.cantidad);
+            precioTotal = parseFloat(precioTotal + this.totprod);
         }
-
+       //REVISAR PORQUÉ EL PRECIO TOTAL SALE COMO UNDEFINED
     }
 
     productos.push(new Producto(prodIng, precIng, cantIng));
 
-    prodgen()
+
+    prodGen()
 }
 
 
 
-//Función constructora y modificadora de productos previamente creados.
 
-function prodgen() {
-
-    prodIng = document.getElementById("cantProd").value = "";
-    precIng = document.getElementById("precIngr").value = "";
-    cantIng = document.getElementById("cantIngr").value = "";
-
-    let genTab = document.createElement("tr");
-
-    for (const producto of productos) {
-
-        genTab.innerHTML = `
-    <td>${producto.nombre}</td>
-    <td>${producto.precio}</td>
-    <td class="canti">
-      <div class="input-group input-group-sm ">
-        <span class="input-group-text" id="basic-addon1">X</span>
-        <input type="number" class="form-control text-center" placeholder="1" aria-describedby="basic-addon1">
-      </div>
-    </td>
-    <td>${producto.cantidad}</td>
-    <td>
-      <div class="d-flex justify-content-end gap-2">
-        <button class="btn btn-sm btn-outline-success" type="submit">Edit</button>
-        <button class="btn btn-sm btn btn-outline-secondary" type="submit">Del</button>
-      </div>
-    </td>
-    `
-    }
-
-    tabGen = document.getElementById("tabla");
-    tabGen.append(genTab);
-
-
-}
+//AL PRESIONAR EL BOTON DE AGREGAR PRODUCTO, DESAPARECE Y APARECEN LOS INPUTS.
 
 function btnAgregar() {
     document.getElementById("agregarProd").style.display = "none";
     document.getElementById("ingresarProd").style.display = "flex"
 
     document.getElementById("cantProd").focus();
+    document.getElementById("cantProd").select();
 }
+
+let agrProBtn = document.getElementById("agrProBtn")
+
+agrProBtn.onclick = () => {btnAgregar()}
+
+
+//AL TERMINAR DE AGREGAR EL PRODUCTO Y ENVIARLO AL ARRAY, OCULTA LOS IMPUTS Y VUELVE A MOSTRAR EL BOTON AGREGAR
 
 function btnAgregarOcul() {
     document.getElementById("agregarProd").style.display = "block";
     document.getElementById("ingresarProd").style.display = "none"
+
+    addProd() 
 }
+
+let ocuProBtn = document.getElementById("button-addon2")
+
+ocuProBtn.onclick = () =>{btnAgregarOcul()}
+
+
